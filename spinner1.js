251522531829533befb0ff49c067fe5e06ececf7
Sid Natarajan <sid.nat@gmail.com>
2022-10-25 20:41:04 +0000

    eslint

--- spinner1.js
+++ spinner1.js
@@ -12,26 +12,26 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 700);
 
 setTimeout(() => {
-  process.stdout.write('\r|   '); 
+  process.stdout.write('\r|   ');
 }, 900);
 
 setTimeout(() => {
-  process.stdout.write('\r/   '); 
+  process.stdout.write('\r/   ');
 }, 1100);
 
 setTimeout(() => {
-  process.stdout.write('\r-   '); 
+  process.stdout.write('\r-   ');
 }, 1300);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 1500);
 
 setTimeout(() => {
-  process.stdout.write('\r|   ' + '\n'); 
+  process.stdout.write('\r|   ' + '\n');
 }, 1700);
--- spinner2.js
+++ spinner2.js
@@ -1,14 +1,14 @@
-const spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|']
-let counter = 100
+const spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+let counter = 100;
 
 for (let symbol of spinner) {
   setTimeout(() => {
     process.stdout.write(`\r${symbol}   `);
   }, counter);
 
-  counter += 200
+  counter += 200;
 }
 
 setTimeout(() => {
   process.stdout.write('\n');
-}, counter)
+}, counter);
